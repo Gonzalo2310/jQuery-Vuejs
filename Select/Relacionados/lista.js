2dab333ae024611d9322c9b86558675a1f93b450
infogon <godan2310@yahoo.es>
2020-06-06 23:13:04 +0200

    Select relacionados

---
+++ Select/Relacionados/lista.js
@@ -0,0 +1,112 @@
+const listaTotal = [
+  {
+    titulo: 'Animales',
+    index: 1,
+    child: [
+      {
+        titulo: 'Gato',
+        index: 101
+      },
+      {
+        titulo: 'Perro',
+        index: 102
+      },
+      {
+        titulo: 'Murciélago',
+        index: 103
+      },
+    ]
+  },
+  {
+    titulo: 'Frutas',
+    index: 2,
+    child: [
+      {
+        titulo: 'Albaricoque',
+        index: 201
+      },
+      {
+        titulo: 'Cereza',
+        index: 202
+      },
+      {
+        titulo: 'Fresa',
+        index: 203
+      },
+      {
+        titulo: 'Mandarina',
+        index: 204
+      },
+      {
+        titulo: 'Manzana',
+        index: 205
+      },
+      {
+        titulo: 'Melocotón',
+        index: 206
+      },
+      {
+        titulo: 'Melón',
+        index: 207
+      },
+      {
+        titulo: 'Naranja',
+        index: 208
+      },
+      {
+        titulo: 'Níspero',
+        index: 209
+      },
+      {
+        titulo: 'Pera',
+        index: 210
+      },
+      {
+        titulo: 'Piña',
+        index: 211
+      },
+      {
+        titulo: 'Plátano',
+        index: 212
+      },
+      {
+        titulo: 'Pomelo',
+        index: 213
+      },
+      {
+        titulo: 'Sandía',
+        index: 214
+      },
+      {
+        titulo: 'Uva',
+        index: 215
+      }
+    ]
+  },
+  {
+    titulo: 'Colores',
+    index: 3,
+    child: [
+      {
+        titulo: 'Rojo',
+        index: 301
+      },
+      {
+        titulo: 'Azul',
+        index: 302
+      },
+      {
+        titulo: 'Verde',
+        index: 303
+      },
+      {
+        titulo: 'Amarillo',
+        index: 304
+      },
+      {
+        titulo: 'Violeta',
+        index: 305
+      }
+    ]
+  }
+]
