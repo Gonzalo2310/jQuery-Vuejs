99f876a0ccee621194ee5ad38483a3cf2899d131
infogon <godan2310@yahoo.es>
2020-06-07 16:19:36 +0200

    Select Filtro

---
+++ Select/Filtro/lista.js
@@ -0,0 +1,62 @@
+const listaTotal = [
+  {
+    titulo: 'Albaricoque',
+    index: 201
+  },
+  {
+    titulo: 'Cereza',
+    index: 202
+  },
+  {
+    titulo: 'Fresa',
+    index: 203
+  },
+  {
+    titulo: 'Mandarina',
+    index: 204
+  },
+  {
+    titulo: 'Manzana',
+    index: 205
+  },
+  {
+    titulo: 'Melocotón',
+    index: 206
+  },
+  {
+    titulo: 'Melón',
+    index: 207
+  },
+  {
+    titulo: 'Naranja',
+    index: 208
+  },
+  {
+    titulo: 'Níspero',
+    index: 209
+  },
+  {
+    titulo: 'Pera',
+    index: 210
+  },
+  {
+    titulo: 'Piña',
+    index: 211
+  },
+  {
+    titulo: 'Plátano',
+    index: 212
+  },
+  {
+    titulo: 'Pomelo',
+    index: 213
+  },
+  {
+    titulo: 'Sandía',
+    index: 214
+  },
+  {
+    titulo: 'Uva',
+    index: 215
+  }
+]
