6b4e6aec38979f4cca9261ed023753008b5dae96
infogon <godan2310@yahoo.es>
2020-06-24 05:55:34 +0200

    Galeria

---
+++ Imagenes/Galeria/links.js
@@ -0,0 +1,12 @@
+const listUrls = [
+  './img/autumn-1072818_640.jpg',
+  './img/autumn-1072827_640.jpg',
+  './img/fall-3089990_640.jpg',
+  './img/green-1072828_640.jpg',
+  './img/natural-vase-1345744_640.jpg',
+  './img/nature-3078078_640.jpg',
+  './img/season-1345752_640.jpg',
+  './img/spider-1345743_640.jpg',
+  './img/sun-1072826_640.jpg',
+  './img/water-1788712_640.jpg'
+]
